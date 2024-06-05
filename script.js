44dbe96de440fb8625926abfef2e03ded59c35b5
Aditya <ap457codes@gmail.com>
2024-06-05 23:03:02 +0530

    Added sounds for piano keys Added functionality for piano keys on pressing, and clicking

---
+++ script.js
@@ -0,0 +1,35 @@
+const pianoKeys = document.querySelectorAll(".key");
+
+let audio = new Audio("./Sounds/a.wav");
+
+function playSound(key){
+    audio.src = `./Sounds/${key}.wav`;
+    audio.play();
+
+    const abc = document.querySelector(`[data-key="${key}"]`);
+    abc.classList.add("active");
+    
+    setTimeout( () => {
+        abc.classList.toggle("active");
+    },150)
+}
+
+function clickedKey(key) {
+    audio.src = `./Sounds/${key}.wav`;
+    audio.play();
+    const abc = document.querySelector(`[data-key="${key}"]`);
+    abc.classList.add("active");
+    
+    setTimeout( () => {
+        abc.classList.toggle("active");
+    },150)
+}
+
+pianoKeys.forEach(key => {
+    console.log(key.dataset.key)
+    key.addEventListener("click", () => playSound(key.dataset.key));
+})
+
+document.addEventListener("keydown", (e) => {
+    clickedKey(e.key);
+}) 
